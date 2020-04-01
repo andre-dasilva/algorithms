2e3397a5892dba9d66719fd99fffe3d7409d24b8
André da Silva <andre-da.silva@outlook.com>
2020-04-01 17:11:41 +0200

    Add python singly linked list algorithm
    Also update .gitignore

---
+++ python/algorithms/singly_linked_list.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+from typing import Any, Optional
+
+
+class Node:
+    def __init__(self, data: Any):
+        self.data: Any = data
+        self.next: Node = None
+
+    def __repr__(self):
+        return repr(self.data)
+
+
+class LinkedList:
+    def __init__(self, head: Node = None):
+        self.head: Node = None
+        self.size: int = 0
+
+    def append(self, node: Node) -> LinkedList:
+        self.size += 1
+        if self.first() is None:
+            self.head = node
+            return self
+
+        end = self.last()
+        end.next = node
+        return self
+
+    def remove(self, node: Node) -> None:
+        curr = self.head
+        prev = None
+
+        while curr is not None and curr != node:
+            prev = curr
+            curr = curr.next
+
+        if prev is None:
+            self.head = curr.next
+        elif curr:
+            prev.next = curr.next
+            curr.next = None
+            self.size -= 1
+
+    def first(self):
+        return self.head
+
+    def last(self):
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        return curr
+
+    def search(self, data: Any) -> Optional[Node]:
+        curr = self.head
+        while curr is not None:
+            if curr.data == data:
+                return curr
+            curr = curr.next
+        return None
+
+    def is_empty(self):
+        return self.size == 0
---
+++ python/tests/test_singly_linked_list.py
@@ -0,0 +1,87 @@
+from python.algorithms.singly_linked_list import LinkedList, Node
+
+
+def test_initialization():
+    slinked_list = LinkedList()
+    assert slinked_list.size == 0
+    assert slinked_list.head is None
+
+
+def test_append_one():
+    slinked_list = LinkedList()
+    node = Node("String as test data")
+    slinked_list.append(node)
+    assert slinked_list.head == node
+
+
+def test_append_multiple():
+    slinked_list = LinkedList()
+    node1 = Node("test 1")
+    node2 = Node("test 2")
+    node3 = Node("test 3")
+
+    slinked_list.append(node1).append(node2).append(node3)
+    assert slinked_list.size == 3
+    assert slinked_list.first() == node1
+    assert slinked_list.last() == node3
+
+
+def test_search_existing():
+    slinked_list = LinkedList()
+    node1 = Node("test 1")
+    node2 = Node("test 2")
+    node3 = Node("test 3")
+
+    slinked_list.append(node1).append(node2).append(node3)
+
+    assert slinked_list.search("test 2") == node2
+
+
+def test_search_not_existing():
+    slinked_list = LinkedList()
+    node1 = Node("test 1")
+    node2 = Node("test 2")
+    node3 = Node("test 3")
+
+    slinked_list.append(node1).append(node2).append(node3)
+
+    assert slinked_list.search("test 4") is None
+
+
+def test_remove_first():
+    slinked_list = LinkedList()
+    node1 = Node("test 1")
+    node2 = Node("test 2")
+    node3 = Node("test 3")
+
+    slinked_list.append(node1).append(node2).append(node3)
+    slinked_list.remove(node1)
+    assert slinked_list.first() == node2
+    assert slinked_list.last() == node3
+
+
+def test_remove_last():
+    slinked_list = LinkedList()
+    node1 = Node("test 1")
+    node2 = Node("test 2")
+    node3 = Node("test 3")
+
+    slinked_list.append(node1).append(node2).append(node3)
+    slinked_list.remove(node3)
+    assert slinked_list.first() == node1
+    assert slinked_list.last() == node2
+
+
+def test_remove_between():
+    slinked_list = LinkedList()
+    node1 = Node("test 1")
+    node2 = Node("test 2")
+    node3 = Node("test 3")
+    node4 = Node("test 4")
+    node5 = Node("test 5")
+
+    slinked_list.append(node1).append(node2).append(
+        node3).append(node4).append(node5)
+    slinked_list.remove(node3)
+    assert slinked_list.first() == node1
+    assert slinked_list.last() == node5
