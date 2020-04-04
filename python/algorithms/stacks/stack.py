30d3cc968c4a252108d65dd4bccac26256940dab
André da Silva <andre-da.silva@outlook.com>
2020-04-04 17:38:54 +0200

    Add stack

---
+++ python/algorithms/stacks/stack.py
@@ -0,0 +1,32 @@
+from typing import Any, List
+
+
+class Node:
+    def __init__(self, data: Any):
+        self.data = data
+
+
+class Stack:
+    def __init__(self):
+        self.stack: List[Node] = []
+        self.size: int = 0
+
+    def is_empty(self) -> bool:
+        return self.size == 0
+
+    def push(self, node: Node) -> None:
+        self.stack.append(node)
+        self.size += 1
+
+    def top(self) -> Node:
+        if self.is_empty():
+            return None
+        return self.stack[self.size - 1]
+
+    def pop(self) -> Node:
+        if self.is_empty():
+            return None
+        node = self.stack[self.size - 1]
+        self.stack.remove(node)
+        self.size -= 1
+        return node
